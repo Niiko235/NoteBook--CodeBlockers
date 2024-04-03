5811bbe7d858341164699d42934cc26773b3ba4d
Juan Camilo Guzmán <juan.guzman@udla.edu.co>
2024-04-03 02:27:05 -0500

    Primer commit de archivo de C++

---
+++ Solucionario/A_Dia3BusquedaBinaria.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+ * Autor: madisktral
+ *
+*/
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+#define vi vector<int>
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) int(x.size())
+#define all(x) sort(x.begin(), x.end());
+#define forn(i, a) for (ll i = 0; i < ll(a); ++i)
+#define nline '\n'
+#define cou(x) cout << x << nline;
+ 
+template <typename T>
+void show(vector<T> a){for (T i: a) cerr << i << " ";}
+template <typename T>
+void fill(vector<T> &a){for (T &i: a) cin >> i;}
+
+void solve(){
+	cout << "Hello world" << '\n';
+}
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+	
+	
+	solve();
+	return 0;
+}
